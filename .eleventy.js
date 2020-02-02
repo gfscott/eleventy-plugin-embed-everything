e85e9b4183e130f083d9bb0cbb2b0ef5d69e4e9a
Graham F. Scott <gfscott@gmail.com>
2020-02-01 20:01:02 -0500

    accounting for no options

--- .eleventy.js
+++ .eleventy.js
@@ -4,23 +4,22 @@ global.youtube = require('eleventy-plugin-youtube-embed');
 module.exports = function(eleventyConfig, options) {
 
   // every valid embed offered
-  const availableEmbeds = ['instagram', 'youtube'];
+  const validEmbeds = ['instagram', 'youtube'];
   // embeds offered by default
   const defaultEmbeds = ['instagram', 'youtube'];
   // dynamically build default settings
   let defaultOptions = {};
-  availableEmbeds.forEach(function(embed){
+  validEmbeds.forEach(function(embed){
     // this is kind of ugly
     let str = '{"'+ embed + '":{"options":{}}}';
     let obj = JSON.parse(str);
     defaultOptions = Object.assign({}, defaultOptions, obj);
   });
+  defaultOptions.use = options && options.use ? validateEmbeds(options.use) : validateEmbeds(defaultEmbeds);
 
   // combine defaults and user options to configure the plugin
   let pluginConfig = Object.assign(defaultOptions, options);
-  // filter the requested embeds so only valid ones are accepted
-  pluginConfig.use = ( options.use ? validateEmbeds(options.use) : defaultEmbeds );
-  
+
   // for each valid embed being used, call it in eleventy
   pluginConfig.use.forEach(function(embedName){
     eleventyConfig.addPlugin(global[embedName], pluginConfig[embedName].options);
@@ -31,9 +30,9 @@ module.exports = function(eleventyConfig, options) {
   function validateEmbeds(arr){
     let out = [];
     arr.forEach(str => {
-      if(availableEmbeds.indexOf(str) > -1){
+      if(validEmbeds.indexOf(str) > -1){
         out.push(str);
-        console.log("✅  Embeds are active for “" + str + "”.");
+        console.info("✅  Embeds are active for “" + str + "”.");
       } else {
         console.error("🛑  Sorry, “" + str + "” is not supported by this plugin right now.");
       }
