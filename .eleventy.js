42d3d45a3a031c97f9f6e13ed308152d126c93dc
Graham F. Scott <gfscott@gmail.com>
2020-06-06 12:46:14 -0400

    safer outputPath

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,23 @@
-const patternPresent = require('./lib/spotPattern.js');
+const spotPattern = require('./lib/spotPattern.js');
 const extractMatches = require('./lib/extractMatches.js');
 const buildEmbed = require('./lib/buildEmbed.js');
 const pluginDefaults = require('./lib/pluginDefaults.js');
 
 module.exports = function(eleventyConfig, options) {
   const pluginConfig = Object.assign(pluginDefaults, options);
-  eleventyConfig.addTransform("embedTikTok", async (content, outputPath) => {
-    if (!outputPath.endsWith(".html")) {
+  eleventyConfig.addTransform("embedVimeo", async (content, outputPath) => {
+    if (outputPath && outputPath.endsWith(".html")) {
+      let matches = spotPattern(content);
+      if (!matches) {
+        return content;
+      }
+      matches.forEach(function (stringToReplace, index) {
+        let media = extractMatches(stringToReplace);
+        let embedCode = buildEmbed(media, pluginConfig, index);
+        content = content.replace(stringToReplace, embedCode);
+      });
       return content;
     }
-    let matches = patternPresent(content);
-    if (!matches) {
-      return content;
-    }
-    // index is used to limit the number of script tags added to each page to one
-    matches.forEach(function(stringToReplace, index) {
-      let media = extractMatches(stringToReplace);
-      let embedCode = buildEmbed(media, pluginConfig, index);
-      content = content.replace(stringToReplace, embedCode);
-    });
     return content;
   });
 };
