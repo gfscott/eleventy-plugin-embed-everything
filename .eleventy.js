c3aac29c140af38d1a7e102c63c964e38a1c5724
Graham F. Scott <gfscott@gmail.com>
2020-06-26 12:35:28 -0400

    first phase
    working, some quirks
    
    fix tests, wrapper div
    
    tests

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 const patternPresent = require('./lib/spotPattern.js');
 const extractVideoId = require('./lib/extractMatches.js');
 const buildEmbedCodeString = require('./lib/buildEmbed.js');
-const pluginDefaults = require('./lib/pluginDefaults.js');
+const { pluginDefaults } = require('./lib/pluginDefaults.js');
 
 module.exports = function (eleventyConfig, options) {
   const pluginConfig = Object.assign(pluginDefaults, options);
@@ -11,9 +11,9 @@ module.exports = function (eleventyConfig, options) {
       if (!matches) {
         return content;
       }
-      matches.forEach(function (stringToReplace) {
+      matches.forEach(function (stringToReplace, index) {
         let videoId = extractVideoId(stringToReplace);
-        let embedCode = buildEmbedCodeString(videoId, pluginConfig);
+        let embedCode = buildEmbedCodeString(videoId, pluginConfig, index);
         content = content.replace(stringToReplace, embedCode);
       });
       return content;
