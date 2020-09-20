de1e15a8edca4d153f9bc5a05a864565b9d1e9f0
Graham F. Scott <gfscott@gmail.com>
2020-09-20 18:18:10 -0400

    restructure test architecture

---
+++ test/inc/validStrings.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {type: 'Album', str: 'spotify.com/album/75qojmHz1id8sL2LDR5qVz'},
+    {type: 'Artist', str: 'spotify.com/artist/066X20Nz7iquqkkCW6Jxy6'},
+    {type: 'Playlist', str: 'spotify.com/playlist/7zv2xFPTH1MBynYafuvtCj'},
+    {type: 'Playlist with user ID', str: 'spotify.com/user/gfscott/playlist/7zv2xFPTH1MBynYafuvtCj'},
+    {type: 'Track', str: 'spotify.com/track/3gsCAGsWr6pUm1Vy7CPPob'},
+    {type: 'Podcast episode', str: 'spotify.com/episode/7G5O2wWmch1ciYDPZS4a4O'},
+  ]
