aaac6a0b946c8cd47e4737eaec0c91410a18e964
Graham F. Scott <gfscott@gmail.com>
2021-02-14 19:29:52 -0500

    style and github actions setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,21 @@
 // Needs to be in global context to be accessed by module
-global.instagram = require('eleventy-plugin-embed-instagram');
-global.soundcloud = require('eleventy-plugin-embed-soundcloud');
-global.spotify = require('eleventy-plugin-embed-spotify');
-global.tiktok = require('eleventy-plugin-embed-tiktok');
-global.twitch = require('eleventy-plugin-embed-twitch');
-global.twitter = require('eleventy-plugin-embed-twitter');
-global.vimeo = require('eleventy-plugin-vimeo-embed');
-global.youtube = require('eleventy-plugin-youtube-embed');
+global.instagram = require("eleventy-plugin-embed-instagram");
+global.soundcloud = require("eleventy-plugin-embed-soundcloud");
+global.spotify = require("eleventy-plugin-embed-spotify");
+global.tiktok = require("eleventy-plugin-embed-tiktok");
+global.twitch = require("eleventy-plugin-embed-twitch");
+global.twitter = require("eleventy-plugin-embed-twitter");
+global.vimeo = require("eleventy-plugin-vimeo-embed");
+global.youtube = require("eleventy-plugin-youtube-embed");
 
-const configOptions = require('./lib/configOptions.js');
+const configOptions = require("./lib/configOptions.js");
 
 module.exports = function(eleventyConfig, options = {}) {
-  let config = configOptions(options);
-  for ( const pluginHandle of config.activePlugins ){
-    eleventyConfig.addPlugin(
-      global[pluginHandle],
-      config.activePluginOptions[pluginHandle].options
-    );
-  }
+	let config = configOptions(options);
+	for (const pluginHandle of config.activePlugins) {
+		eleventyConfig.addPlugin(
+			global[pluginHandle],
+			config.activePluginOptions[pluginHandle].options,
+		);
+	}
 };
--- test/test-validatePlugins.js
+++ test/test-validatePlugins.js
@@ -1,65 +1,65 @@
-const test = require('ava');
-const validatePlugins = require('../lib/validatePlugins.js');
-const { defaultPlugins, validPlugins } = require('../lib/pluginDefaults.js');
+const test = require("ava");
+const validatePlugins = require("../lib/validatePlugins.js");
+const {defaultPlugins, validPlugins} = require("../lib/pluginDefaults.js");
 
 test(
-  `Validator returns expected default plugin array`,
-  (t) => {
-    output = validatePlugins(defaultPlugins);
-    t.deepEqual(output, defaultPlugins);
-  },
+	"Validator returns expected default plugin array",
+	(t) => {
+		let output = validatePlugins(defaultPlugins);
+		t.deepEqual(output, defaultPlugins);
+	},
 );
 
 test(
-  `Validator rejects invalid plugin strings, returns defaults`,
-  (t) => {
-    let list = [...defaultPlugins, 'wrong'];
-    output = validatePlugins(list);
-    t.deepEqual(output, defaultPlugins);
-  },
+	"Validator rejects invalid plugin strings, returns defaults",
+	(t) => {
+		let list = [...defaultPlugins, "wrong"];
+		let output = validatePlugins(list);
+		t.deepEqual(output, defaultPlugins);
+	},
 );
 
 test(
-  `Validator accepts valid non-default plugin string`,
-  (t) => {
-    let list = [...defaultPlugins, 'soundcloud'];
-    output = validatePlugins(list);
-    t.deepEqual(output, validPlugins);
-  },
+	"Validator accepts valid non-default plugin string",
+	(t) => {
+		let list = [...defaultPlugins, "soundcloud"];
+		let output = validatePlugins(list);
+		t.deepEqual(output, validPlugins);
+	},
 );
 
 test(
-  `Validator returns non-alphabetical list in alphabetical order`,
-  (t) => {
-    let list = ['youtube', 'vimeo', 'twitter'];
-    output = validatePlugins(list);
-    t.deepEqual(output, ['twitter', 'vimeo', 'youtube']);
-  },
+	"Validator returns non-alphabetical list in alphabetical order",
+	(t) => {
+		let list = ["youtube", "vimeo", "twitter"];
+		let output = validatePlugins(list);
+		t.deepEqual(output, ["twitter", "vimeo", "youtube"]);
+	},
 );
 
 test(
-  `Validator returns expected valid plugin list subset`,
-  (t) => {
-    let list = ['instagram', 'soundcloud', 'spotify'];
-    output = validatePlugins(list);
-    t.deepEqual(output, list);
-  },
+	"Validator returns expected valid plugin list subset",
+	(t) => {
+		let list = ["instagram", "soundcloud", "spotify"];
+		let output = validatePlugins(list);
+		t.deepEqual(output, list);
+	},
 );
 
 test(
-  `Validator returns expected subset, but rejects invalid plugin`,
-  (t) => {
-    let list = ['instagram', 'spotify', 'wrong'];
-    output = validatePlugins(list);
-    t.deepEqual(output, ['instagram', 'spotify']);
-  },
+	"Validator returns expected subset, but rejects invalid plugin",
+	(t) => {
+		let list = ["instagram", "spotify", "wrong"];
+		let output = validatePlugins(list);
+		t.deepEqual(output, ["instagram", "spotify"]);
+	},
 );
 
 test(
-  `Validator rejects non-string array items`,
-  (t) => {
-    let list = [false, 'instagram', 'spotify', true, 123, {foo: 'bar'}];
-    output = validatePlugins(list);
-    t.deepEqual(output, ['instagram', 'spotify']);
-  },
+	"Validator rejects non-string array items",
+	(t) => {
+		let list = [false, "instagram", "spotify", true, 123, {foo: "bar"}];
+		let output = validatePlugins(list);
+		t.deepEqual(output, ["instagram", "spotify"]);
+	},
 );
