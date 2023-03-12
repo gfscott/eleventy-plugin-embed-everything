292c2022c160e07af570e3d3fe2f94d1cb1f96be
Graham F. Scott <gfscott@gmail.com>
2023-03-12 15:18:15 -0400

    configurability

--- packages/ted/lib/replace.js
+++ packages/ted/lib/replace.js
@@ -1,14 +1,10 @@
 module.exports = function(match, config) {
-  // read URL
-  // get URL information
-  // massage the URL data
-  // build the iframe
+  const {pathname} = new URL(match[3]);
+  const embedUrl = `https://embed.ted.com${pathname}`
   
-  const {pathname: path} = new URL(match[3]);
-  const embedUrl = `https://embed.ted.com${path}`
+  let embed = `<div class="${config.embedClass}" style="${config.containerCss}">`
+  embed += `<iframe src="${embedUrl}" style="${config.iframeCss}" width="${config.iframeWidth}" height="${config.iframeHeight}" frameborder="${config.iframeFrameborder}" scrolling="${config.iframeScrolling}" ${config.allowFullscreen ? 'allowfullscreen' : ''}></iframe>`
+  embed += "</div>"
   
-  
-  return `<iframe class="${config.embedClass}" src="${embedUrl}" width="854" height="480" frameborder="0" scrolling="no" allowfullscreen></iframe>`
-
-  // <div style="max-width:854px"><div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://embed.ted.com/talks/lang/en/mesmin_destin_how_everyday_interactions_shape_your_future" width="854" height="480" style="position:absolute;left:0;top:0;width:100%;height:100%" frameborder="0" scrolling="no" allowfullscreen></iframe></div></div>
+  return embed;
 }
