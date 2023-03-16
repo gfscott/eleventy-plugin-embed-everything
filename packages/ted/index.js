5b44da36ac74de27566570359b4badb3dac4fdd2
Graham F. Scott <gfscott@gmail.com>
2023-03-16 18:48:14 -0400

    comment

--- packages/ted/index.js
+++ packages/ted/index.js
@@ -11,8 +11,14 @@ module.exports = function(eleventyConfig, options = {}) {
     if ( !outputPath || !outputPath.endsWith(".html")) {
       return content;
     }
-    // 1. Use the spread operator for the replace function parameter to allow an arbitrary number of capturing groups
-    // 2. Use an anonymous function to immediately pass the full matches array to a function that can also accept the embed config object.
+    /**
+     * 1. Use the spread operator for the replace function parameter
+     *    to get an array with an arbitrary number of capturing groups
+     * 2. Use an anonymous function to immediately pass the full 
+     *    matches array to a function that can also accept the embed
+     *    config object. The value returned by this function will
+     *    replace the matched string.
+     */
     return content.replace(pattern, (...match) => replace(match, config));
   });
 };
