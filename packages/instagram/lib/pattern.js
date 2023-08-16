67fdb5d3e1b57d210dcdd9c32e53f4deaf1d0e5f
Graham F. Scott <gfscott@gmail.com>
2023-08-16 09:16:31 -0400

    refactor, not yet ready

--- packages/instagram/lib/pattern.js
+++ packages/instagram/lib/pattern.js
@@ -1,4 +1,1 @@
-const pattern = /<p>(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:\/\/)?(?:w{3}\.)?(?:instagram\.com\/p\/)(?:[0-9a-zA-Z_-]{11})[^\s]*?(?=(\s*))\3(?:<\/a>)??(?=(\s*))\4<\/p>/g;
-module.exports = function(str) {
-  return str.match(pattern);
-}
+module.exports = /<p>(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:)??(?:\/\/)??(?:w{3}\.)?instagram\.com\/p\/([0-9a-zA-Z_-]{11})[^\s]*?(?=(\s*))\4(?:<\/a>)??(?=(\s*))\5<\/p>/g;
--- packages/instagram/test/_validUrls.js
+++ packages/instagram/test/_validUrls.js
@@ -1,6 +1,6 @@
 /**
  * VALID URLS
- * 
+ *
  * Starting with a short list of valid URL fragments, return
  * all possible valid URL permutations.
  */
@@ -9,24 +9,19 @@ const permute = require('./_permuteArrays.js');
 
 /**
  * Core URL structures accepted by the plugin
- * Domain and path only 
+ * Domain and path only
  */
 const validUrls = [
- 'ted.com/talks/fake_talk',
+ 'instagram.com/p/0123456789a',
 ]
 
 
 /**
  * Non-core URL structures accepted by the plugin
  * Various protocol variants and URL parameters
- * 
- * Note: With TED, we test for a trailing "/c" because they use it
- * for tracking internal site navigation (such as from the home page
- * or recommendations in the sidebar). But embed.ted.com URLs that
- * include this fake path directory fail to load.
  */
 const validPrefixes = ['', '//', 'http://', 'https://', 'www.', 'http://www.', 'https://www.']
-const validSuffixes = ['/', '/c', '?', '%3F', '?foo', '%3Ffoo', '?foo=bar', '%3Ffoo%3Dbar']
+const validSuffixes = ['/', '?', '%3F', '?foo', '%3Ffoo', '?foo=bar', '%3Ffoo%3Dbar']
 
 /**
  * Cumulative lists of all URL permutations.
