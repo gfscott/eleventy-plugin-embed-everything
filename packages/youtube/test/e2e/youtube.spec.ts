bfda332f47c4c16ef238a1f9c520e5ffbaad0c94
Graham F. Scott <gfscott@gmail.com>
2023-10-11 17:23:13 -0400

    Make click title to open YouTube the last test so it doesn't block others?

--- packages/youtube/test/e2e/youtube.spec.ts
+++ packages/youtube/test/e2e/youtube.spec.ts
@@ -19,30 +19,28 @@ test('test', async ({ page }) => {
 
   // Expect the iframe to have the correct src
   await expect(embedFrameElement).toHaveAttribute('src', 'https://www.youtube-nocookie.com/embed/hIs5StN8J-0')
-
-
+  
   // Click [aria-label="Play"]
   await embedFrame.locator('[aria-label="Play"]').click();
-
+  
   // Click [aria-label="Play \(k\)"]
   // Started failing for unknown reasons around August 2023
   // await embedFrame.locator('[aria-label="Play \\(k\\)"]').click();
-
+  
   // Click .ytp-fullscreen-button
   // This used to fail on webkit for unknown reasons
   // now it succeeds for unknown reasons
   await embedFrame.locator('.ytp-fullscreen-button').click();
-
+  
   // Click [aria-label="Settings"]
   await embedFrame.locator('[aria-label="Settings"]').click();
-
+  
   // Click [aria-label="Settings"]
   await embedFrame.locator('[aria-label="Settings"]').click();
-
-  // Click text=Animotion - Obsession >> nth=0
-  await embedFrame.locator('text=Animotion - Obsession').first().click()
 
   // Click [aria-label="Copy link"]
   await embedFrame.locator('[aria-label="Copy link"]').click();
-
+  
+  // Click text=Animotion - Obsession >> nth=0
+  await embedFrame.locator('.ytp-title-link').first().click()
 });
