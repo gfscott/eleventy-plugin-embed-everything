2ccdbaca9532c3a833f99ea292f06d73254f080d
Graham F. Scott <gfscott@gmail.com>
2024-01-16 20:09:24 -0500

    Rudimentary proof of concept

---
+++ packages/openstreetmap/lib/defaults.js
@@ -0,0 +1,5 @@
+module.exports = {
+  embedClass: "eleventy-plugin-embed-openstreetmap",
+  height: 500,
+  width: 500,
+}
---
+++ packages/openstreetmap/lib/replace.js
@@ -0,0 +1,53 @@
+module.exports = function(match, config) {
+
+  return "foo";
+
+
+}
+
+
+
+
+
+
+
+function rad2deg(radians) {
+  return radians * (180/Math.PI);
+}
+
+function deg2rad(degrees) {
+  return degrees * (Math.PI/180);
+}
+
+function sec(num) {
+  return 1 / Math.cos(num);
+}
+
+function getLonLat(xtile, ytile, zoom) {
+  const n = Math.pow(2, zoom);
+  const lonDegree = xtile / n * 360.0 - 180.0;
+  const latDegree = rad2deg(Math.atan(Math.sinh(Math.PI * (1 - 2 * ytile / n))));
+  return [lonDegree, latDegree];
+}
+
+function getTileNumber(lon, lat, zoom) {
+  const xtile = (lon + 180)/360 * Math.pow(2, zoom);
+  const ytile = (1 - Math.log(Math.tan(deg2rad(lat)) + sec(deg2rad(lat)))/Math.PI)/2 * Math.pow(2,zoom);
+  return [xtile, ytile];
+}
+
+module.exports.longLatToBbox = function lonLatToBbox(lon, lat, zoom, embedWidth, embedHeight) {  
+  const [xtile, ytile] = getTileNumber(parseFloat(lon), parseFloat(lat), zoom);
+  const tileSize = 256;
+
+	const xtile_s = (xtile * tileSize - embedWidth) / tileSize;
+	const ytile_s = (ytile * tileSize - embedHeight) / tileSize;
+	const xtile_e = (xtile * tileSize + embedWidth) / tileSize;
+	const ytile_e = (ytile * tileSize + embedHeight) / tileSize;
+
+	const [lon_s, lat_s] = getLonLat(xtile_s, ytile_s, zoom);
+	const [lon_e, lat_e] = getLonLat(xtile_e, ytile_e, zoom);
+  
+  return `${lon_s},${lat_e},${lon_e},${lat_s}`;
+}
+
