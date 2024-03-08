e9d418b03acc7b778a6cd963db6b96e0e04b2ea1
Graham F. Scott <gfscott@gmail.com>
2024-03-07 19:48:46 -0500

    Complete URL pattern tests

--- packages/openstreetmap/test/pattern.test.mjs
+++ packages/openstreetmap/test/pattern.test.mjs
@@ -1,19 +1,28 @@
 import test from 'ava';
 import pattern from '../lib/pattern.js';
+import permuteArrays from 'permute-arrays';
 
-// const str = '<p>https://www.openstreetmap.org/#map=8/46.195/-81.362</p>';
-// const str = '<p>https://www.openstreetmap.org/way/1147323572#map=8/46.195/-81.362</p>';
-const str = '<p>https://www.openstreetmap.org/search?whereami=1&query=52.147%2C104.106#map=8/46.195/-81.362</p>';
+const base = 'openstreetmap.org';
+const prefixes = ['', '//', 'www.', '//www.', 'https://', 'http://', 'https://www.', 'http://www.'];
+const suffixes = [
+  '/#map=8/46.195/-81.362',
+  '/way/1147323572#map=8/46.195/-81.362',
+  '/search?whereami=1&query=52.147%2C104.106#map=8/46.195/-81.362'
+];
+// Use set to remove duplicates; slice to remove URLs without paths
+const validUrls = new Set(permuteArrays(base, prefixes, suffixes).slice(8))
 
-test('Extremely basic regex proof of concept', t => {
-  pattern.lastIndex = 0;
-  t.regex(str, pattern)
-})
+for (let url of validUrls) {
+  test(`${url} is a valid URL`, t => {
+    pattern.lastIndex = 0;
+    t.regex(`<p>${url}</p>`, pattern);
+  });
 
-test('Expected values extracted', t => {
-  pattern.lastIndex = 0;
-  const { groups: { zoom, lat, long } } = pattern.exec(str);
-  t.is(zoom, '8');
-  t.is(lat, '46.195');
-  t.is(long, '-81.362');
-})
+  test(`Expected values extracted from ${url}`, t => {
+    pattern.lastIndex = 0;
+    const { groups: { zoom, lat, long } } = pattern.exec(`<p>${url}</p>`);
+    t.is(zoom, '8');
+    t.is(lat, '46.195');
+    t.is(long, '-81.362');
+  })
+}
