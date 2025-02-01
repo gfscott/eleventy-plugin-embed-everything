bed0f07044de71e7fcf55ecf5469c5a20c4b1472
Jason Shellen <jason@shellen.com>
2025-01-31 17:22:20 -0800

    Instagram Reels and TV urls support

--- packages/instagram/lib/extractMatches.js
+++ packages/instagram/lib/extractMatches.js
@@ -1,29 +1,37 @@
 /**
- * Regular expression to extract the Instagram ID from the URL.
- * 
- * Why out[3]?
- * -----------
- * `\s*`, necessary to accomodate arbitrary whitespace, is 
- * vulnerable to catastrophic backtracking that can lead to 
+ * Regular expression to extract the Instagram media ID and type from the URL.
+ *
+ * Why out[3] and out[4]?
+ * -----------------------
+ * `\s*`, necessary to accommodate arbitrary whitespace, is
+ * vulnerable to catastrophic backtracking that can lead to
  * regular expression denial-of-service. JS/Node doesn’t support
  * RegEx atomic groups like other languages do. The workaround
  * requires capturing groups for whitespace with positive
  * lookaheads:
- * 
+ *
  * https://blog.stevenlevithan.com/archives/mimic-atomic-groups
- * 
- * Until we formally drop support for Node 8 and can start using
+ *
+ * Until we formally drop support for older Node versions and can start using
  * named capture groups, we have to count matches in the returned
  * RegEx array.
- *  
+ *
  * out[0]           = full match
- * out[1], out[2]   = optional whitespace characters 
- * out[3]           = Instagram media ID
- * out[4], out[5]   = optional whitespace characters
+ * out[1], out[2]   = optional whitespace characters
+ * out[3]           = Instagram media type (p|reel|tv)
+ * out[4]           = Instagram media ID
+ * out[5], out[6]   = optional whitespace characters
  */
 
-const pattern = /<p>(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:\/\/)?(?:w{3}\.)?(?:instagram\.com)\/(?:p\/)?([0-9a-zA-Z-_]{11})(?:\S*)(?=(\s*))\4(?:<\/a>)?(?=(\s*))\5<\/p>/;
-module.exports = function(str) {
-  let out = pattern.exec(str);
-  return out[3];
-}
+const pattern =
+	/<p>(?=(\s*))\1(?:<a [^>]*?>)??(?=(\s*))\2(?:https?:\/\/)?(?:w{3}\.)?(?:instagram\.com)\/(p|reel|tv)\/([0-9a-zA-Z_-]+)(?:\/.*)?(?=(\s*))\5(?:<\/a>)?(?=(\s*))\6<\/p>/;
+
+module.exports = function (str) {
+	let out = pattern.exec(str);
+	if (!out) return null;
+
+	return {
+		type: out[3], // p, reel, or tv
+		id: out[4], // Instagram media ID
+	};
+};
--- packages/instagram/test.js
+++ packages/instagram/test.js
@@ -4,17 +4,28 @@ const extractId = require('./lib/extractMatches.js');
 const buildEmbed = require('./lib/buildEmbed.js');
 const pluginDefaults = require('./lib/pluginDefaults.js');
 
-
+/**
+ * Valid strings to test
+ */
 const validStrings = [
-  {type: 'Standard', str: 'https://www.instagram.com/p/B-rRt1MjKZD/'},
-  {type: 'With http', str: 'http://www.instagram.com/p/B-rRt1MjKZD/'},
-  {type: 'Without protocol', str: 'www.instagram.com/p/B-rRt1MjKZD/'},
-  {type: 'Without trailing slash', str: 'https://www.instagram.com/p/B-rRt1MjKZD'},
-  {type: 'With https, without www', str: 'https://instagram.com/p/B-rRt1MjKZD/'},
-  {type: 'With http, without www', str: 'https://www.instagram.com/p/B-rRt1MjKZD/'},
-  {type: 'Without https, without www', str: 'instagram.com/p/B-rRt1MjKZD/'},
+  {type: 'Standard', str: 'https://www.instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'With http', str: 'http://www.instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'Without protocol', str: 'www.instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'Without trailing slash', str: 'https://www.instagram.com/p/B-rRt1MjKZD', expectedType: 'p'},
+  {type: 'With https, without www', str: 'https://instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'With http, without www', str: 'https://www.instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'Without https, without www', str: 'instagram.com/p/B-rRt1MjKZD/', expectedType: 'p'},
+  {type: 'Standard Reel', str: 'https://www.instagram.com/reel/DE0T298yYMj/', expectedType: 'reel'},
+  {type: 'Without protocol Reel', str: 'www.instagram.com/reel/DE0T298yYMj/', expectedType: 'reel'},
+  {type: 'Without www Reel', str: 'instagram.com/reel/DE0T298yYMj/', expectedType: 'reel'},
+  {type: 'Standard TV', str: 'https://www.instagram.com/tv/BkQjCfsBIzi/', expectedType: 'tv'},
+  {type: 'Without protocol TV', str: 'www.instagram.com/tv/BkQjCfsBIzi/', expectedType: 'tv'},
+  {type: 'Without www TV', str: 'instagram.com/tv/BkQjCfsBIzi/', expectedType: 'tv'}
 ]
 
+/**
+ * Invalid strings to test
+ */
 const invalidStrings = [
   {type: 'Incomplete photo ID', str: 'https://www.instagram.com/p/abcde/'},
   {type: 'With prepended text', str: 'foo https://www.instagram.com/p/B-rRt1MjKZD/'},
@@ -59,23 +70,30 @@ validStrings.forEach(function(obj){
     t.truthy(patternPresent(withLinksAndWhitespace));
   });
 });
+
 /**
  * Ensure that valid strings produce the expected media ID as output
  */
 validStrings.forEach(function(obj){
   test(`Proper ID returned: ${obj.type} ideal case`, t => {
     let idealCase = `<p>${obj.str}</p>`;
-    t.is(extractId(idealCase), 'B-rRt1MjKZD');
+    let extracted = extractId(idealCase);
+    t.is(extracted.type, obj.expectedType);
+    t.truthy(extracted.id);
   });
   test(`Proper ID returned: ${obj.type} with links`, t => {
     let withLinks = `<p><a href="">${obj.str}</a></p>`;
-    t.is(extractId(withLinks), 'B-rRt1MjKZD');
+    let extracted = extractId(withLinks);
+    t.is(extracted.type, obj.expectedType);
+    t.truthy(extracted.id);
   });
   test(`Proper ID returned: ${obj.type} with whitespace`, t => {
     let withWhitespace = `<p>
       ${obj.str}
     </p>`;
-    t.is(extractId(withWhitespace), 'B-rRt1MjKZD');
+    let extracted = extractId(withWhitespace);
+    t.is(extracted.type, obj.expectedType);
+    t.truthy(extracted.id);
   });
   test(`Proper ID returned: ${obj.type} with links and whitespace`, t => {
     let withLinksAndWhitespace = `<p>
@@ -83,7 +101,9 @@ validStrings.forEach(function(obj){
         ${obj.str}
       </a>
     </p>`;
-    t.is(extractId(withLinksAndWhitespace), 'B-rRt1MjKZD');
+    let extracted = extractId(withLinksAndWhitespace);
+    t.is(extracted.type, obj.expectedType);
+    t.truthy(extracted.id);
   });
 });
 
@@ -93,22 +113,25 @@ validStrings.forEach(function(obj){
 validStrings.forEach(function(obj){
   test(`Proper HTML output, zero-index: ${obj.type} ideal case`, t => {
     let idealCase = `<p>${obj.str}</p>`;
-    t.is(buildEmbed(extractId(idealCase), pluginDefaults, 0),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
+    let extracted = extractId(idealCase);
+    t.is(buildEmbed(extracted, pluginDefaults, 0),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
     );
   });
   test(`Proper HTML output, zero-index: ${obj.type} with links`, t => {
     let withLinks = `<p><a href="">${obj.str}</a></p>`;
-    t.is(buildEmbed(extractId(withLinks), pluginDefaults, 0),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
+    let extracted = extractId(withLinks);
+    t.is(buildEmbed(extracted, pluginDefaults, 0),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
     );
   });
   test(`Proper HTML output, zero-index: ${obj.type} with whitespace`, t => {
     let withWhitespace = `<p>
       ${obj.str}
     </p>`;
-    t.is(buildEmbed(extractId(withWhitespace), pluginDefaults, 0),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
+    let extracted = extractId(withWhitespace);
+    t.is(buildEmbed(extracted, pluginDefaults, 0),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
     );
   });
   test(`Proper HTML output, zero-index: ${obj.type} with links and whitespace`, t => {
@@ -117,33 +140,38 @@ validStrings.forEach(function(obj){
         ${obj.str}
       </a>
     </p>`;
-    t.is(buildEmbed(extractId(withLinksAndWhitespace), pluginDefaults, 0),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
+    let extracted = extractId(withLinksAndWhitespace);
+    t.is(buildEmbed(extracted, pluginDefaults, 0),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote><script async defer src="https://www.instagram.com/embed.js"></script>`
     );
   });
 });
+
 /**
  * Ensure that valid strings pass
  */
 validStrings.forEach(function(obj){
   test(`Proper HTML output, >0 index: ${obj.type} ideal case`, t => {
     let idealCase = `<p>${obj.str}</p>`;
-    t.is(buildEmbed(extractId(idealCase), pluginDefaults, 1),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote>`
+    let extracted = extractId(idealCase);
+    t.is(buildEmbed(extracted, pluginDefaults, 1),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote>`
     );
   });
   test(`Proper HTML output, >0 index: ${obj.type} with links`, t => {
     let withLinks = `<p><a href="">${obj.str}</a></p>`;
-    t.is(buildEmbed(extractId(withLinks), pluginDefaults, 1),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote>`
+    let extracted = extractId(withLinks);
+    t.is(buildEmbed(extracted, pluginDefaults, 1),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote>`
     );
   });
   test(`Proper HTML output, >0 index: ${obj.type} with whitespace`, t => {
     let withWhitespace = `<p>
       ${obj.str}
     </p>`;
-    t.is(buildEmbed(extractId(withWhitespace), pluginDefaults, 1),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote>`
+    let extracted = extractId(withWhitespace);
+    t.is(buildEmbed(extracted, pluginDefaults, 1),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote>`
     );
   });
   test(`Proper HTML output, >0 index: ${obj.type} with links and whitespace`, t => {
@@ -152,8 +180,9 @@ validStrings.forEach(function(obj){
         ${obj.str}
       </a>
     </p>`;
-    t.is(buildEmbed(extractId(withLinksAndWhitespace), pluginDefaults, 1),
-      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/p/B-rRt1MjKZD"></blockquote>`
+    let extracted = extractId(withLinksAndWhitespace);
+    t.is(buildEmbed(extracted, pluginDefaults, 1),
+      `<blockquote class="eleventy-plugin-embed-instagram instagram-media" data-instgrm-permalink="https://www.instagram.com/${extracted.type}/${extracted.id}"></blockquote>`
     );
   });
 });
